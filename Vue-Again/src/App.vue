88869520651ae2564bd56767010616c6b0535044
alizarazot <alizarazot@ufpso.edu.co>
2023-11-08 13:03:14 -0500

    Add progress

--- Vue-Again/src/App.vue
+++ Vue-Again/src/App.vue
@@ -1,47 +1,141 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let newTask = ref('')
+let taskList = ref([
+  { text: 'Estudiar', done: false },
+  { text: 'Dormir', done: true }
+])
+
+function addTask() {
+  if (newTask.value.trim() === '') {
+    newTask.value = ''
+    return
+  }
+
+  taskList.value.push({
+    text: newTask.value,
+    done: false
+  })
+
+  newTask.value = ''
+}
+
+function remove(index) {
+  taskList.value.splice(index, 1)
+}
+
+function setDone(index) {
+  taskList.value[index].done = !taskList.value[index].done
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="card">
+    <h1>To-Do</h1>
+    <p class="subtitle">{{ newTask }}</p>
+    <div>
+      <div
+        v-for="(task, index) in taskList"
+        :key="index"
+        @click="setDone(index)"
+        class="task"
+        :class="{ done: task.done }"
+      >
+        {{ task.text }}<span @click="remove(index)" class="button">X</span>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <input
+        v-model="newTask"
+        @keypress.enter="addTask"
+        type="text"
+        placeholder="Escribe tu tarea..."
+      />
+      <p v-show="newTask != ''" class="help">Presiona "INTRO" para añadir la tarea.</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.done {
+  text-decoration: line-through;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.card {
+  --sky: #78c0e0;
+  --celestial: #449dd1;
+  --navy: #150578;
+  --federal: #0e0e52;
+  --violet: #3943b7;
+
+  font-family: 'Dancing Script', cursive;
+  font-size: 48px;
+
+  background-color: var(--federal);
+  color: white;
+
+  max-width: 520px;
+  border-radius: 8px;
+  padding: 18px 16px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+h1 {
+  text-transform: uppercase;
+  text-align: center;
+  padding: 0;
+  margin: 0;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.subtitle {
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  opacity: 0.6;
+  margin-bottom: 16px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.task {
+  background-color: var(--celestial);
+  border-radius: 4px;
+  padding: 2px 8px;
+  margin-bottom: 2px;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+.task:hover {
+  background-color: var(--sky);
+}
+
+.button {
+  background-color: var(--federal);
+  padding: 0 5px;
+  border-radius: 4px;
+  font-size: 32px;
+  cursor: pointer;
+  line-height: 57px;
+}
+
+.button:hover {
+  background-color: var(--violet);
+}
+
+input {
+  font-family: 'Dancing Script', cursive;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+  padding: 3px 6px;
+  border-radius: 4px;
+  font-size: 48px;
+}
+
+.help {
+  margin: 15px 0 0 0;
+  font-size: 20px;
+  opacity: 0.4;
 }
 </style>
